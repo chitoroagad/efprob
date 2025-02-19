1616dda82c6e2e34c6740a3114721085f87b22e1
LISIFEI <zcabsl0@ucl.ac.uk>
2025-02-19 16:43:53 +0000

    Smoking Scenario

--- src/Smoking.py
+++ src/Smoking.py
@@ -36,19 +36,38 @@ PSTC = State(omega1, sp)
 PC_ST = get_conditional_probability(PSTC, [1, 1, 0], [0, 0, 1])
 PS = PSTC.MM(1, 0, 0)
 PT = PSTC.MM(0, 1, 0)
+PC = PSTC.MM(0, 0, 1)
 pST = PSTC.MM(1, 1, 0)
 
 i = idn(PT.sp)
 i2 = idn(PS.sp)
+i3 = idn(PC.sp)
 copy = copy2(PS.sp)
-
+swap = swap(PS.sp, PC.sp)
+cap = cap(PS.sp)
+cup = cup(PS.sp)
+discard = discard(PS.sp)
+uniform = uniform_state(PS.sp)
+cut = uniform * discard
 
 PS_copy = copy * PS
 y = PS_copy @ i
 x = i2 @ PC_ST
 
 f =  (i2 @ PC_ST) * (PS_copy @ i)
-# f = (PC_ST) * ((PS_copy) @ i)
+g = get_conditional_probability(PSTC, [1, 0, 0], [0, 1, 0])
+print(g)
+print(f)
+def comb_compose(f, g):
+    m = (i2 @ i @ (swap * f)) * (i @ copy) * (i2 @ g) * copy
+    print(copy)
+    print(swap * f)
+    print(m)
+    return (i2 @ i @ i3 @ cap) * (m @ i2) * cup
+
+omega_cut = comb_compose((cut @ i2) * f, g)
+result = get_conditional_probability(omega_cut, [1, 0, 0], [0, 0, 1])
+
 
 # print(i.array)
 # print(PC_ST.array)
@@ -57,4 +76,5 @@ f =  (i2 @ PC_ST) * (PS_copy @ i)
 # print(PS)
 # print(PSTC.array)
 # print(copy.array)
-print(f.array)
+print(omega_cut)
+print(result.array)
